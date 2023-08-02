113cfc18a9a64dc6b4e0fdb3ac6267300766c1d2
s711183122 <s711183122@gm.ntpu.edu.tw>
2023-08-02 10:33:33 +0800

    20230802

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import cv2, os
 from config import *
 import time as ttiimmee
 if not os.path.isdir(output_path): os.mkdir(output_path)
-if not os.path.isdir(output_path_n): os.mkdir(output_path_n)
-if not os.path.isdir(output_path_m): os.mkdir(output_path_m)
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
 class QuickCut:
@@ -19,9 +17,10 @@ class QuickCut:
         self.h = int(self.y1-self.y0)
         self.time = int(time)
         self.type = type
-        self.name = f'{self.type}_quickcut_{self.x0}_{self.y0}_{self.time}_{str(int(ttiimmee.time()))[5:10]}'
-        if self.type == 'n': self.savename = f'{output_path_n}{self.name}.mp4'
-        elif self.type == 'm': self.savename = f'{output_path_m}{self.name}.mp4'
+        self.name = f'{self.type}_quickcut_{ttiimmee.strftime("%m_%d_%Y_%H_%M_%S", ttiimmee.localtime())}_({self.x0}_{self.y0})_({self.x1}_{self.y1})'
+        output_path_type = f'{output_path}{self.type}/'
+        if not os.path.isdir(output_path_type): os.mkdir(output_path_type)
+        self.savename = f'{output_path_type}{self.name}.mp4'
         self.VW = cv2.VideoWriter(self.savename, fourcc, fps, (self.w, self.h))
     def __str__(self) -> str:
         return f'{self.name} L=({self.x0},{self.y0}) R=({self.x1},{self.y1})'
@@ -101,12 +100,12 @@ def main():
             if play: play = 0
             else: play = 1
             print('Play' if play else 'Pause')
-        elif key == ord('n'):
-            print(f'New QuickCut n')
+        elif key in [49,50,51,52,53,54,55,56,57]:
+            print(f'New QuickCut {key-48}')
             data['new_QuickCut'] = 1
             while True:
                 key2 = cv2.waitKey()
-                if key2 == 13 or key2 == ord('n'):
+                if key2 == 13 or key2 == key:
                     if not data['tmp']['L']:
                         print(' You must to set L !')
                     elif not data['tmp']['R']:
@@ -116,29 +115,7 @@ def main():
                         data['tmp']['L'] = None
                         data['tmp']['R'] = None
                     else:
-                        qc = QuickCut(data['tmp'], time=cap.get(1), fps=fps)
-                        data['QuickCut'].append(qc)
-                        print(f'{qc}')
-                        break
-                elif key2 == 27:    break
-            data['new_QuickCut'] = 0
-            data['tmp'] = {'L':None, 'R':None}
-        elif key == ord('m'):
-            print(f'New QuickCut m')
-            data['new_QuickCut'] = 1
-            while True:
-                key2 = cv2.waitKey()
-                if key2 == 13 or key2 == ord('m'):
-                    if not data['tmp']['L']:
-                        print(' You must to set L !')
-                    elif not data['tmp']['R']:
-                        print(' You must to set R !')
-                    elif data['tmp']['L'][0] >= data['tmp']['R'][0] or data['tmp']['L'][1] >= data['tmp']['R'][1]:
-                        print(' L is over R, please try it again.')
-                        data['tmp']['L'] = None
-                        data['tmp']['R'] = None
-                    else:
-                        qc = QuickCut(data['tmp'], time=cap.get(1), fps=fps, type='m')
+                        qc = QuickCut(data['tmp'], time=cap.get(1), fps=fps, type=f'{key}')
                         data['QuickCut'].append(qc)
                         print(f'{qc}')
                         break
